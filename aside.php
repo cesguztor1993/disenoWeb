2b9a14c8d3375204cef2415b5b671ee3d400c056
cesguztor <572417@senati.pe>
2022-07-18 13:53:01 -0500

    18.07.2022 Creando menú

--- aside.php
+++ aside.php
@@ -4,6 +4,7 @@
             <a href="?p=001_flotandoElementos">Propiedad Float</a><br>
             <a href="?p=002_position">Propiedad Position</a><br>
             <a href="?p=003_ejercicio001">Ejercicio001</a><br>
+            <a href="?p=004_creacionMenu">Creación de Menú</a><br>
         </div>
     </div>
 </div>
---
+++ contenido/004_creacionMenu.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="css/004_creacionMenu.css">
+<div class="contenedorMenu">
+    <ul class="menu">
+        <li><a href="#">Electro Oriente</a></li>
+        <li><a href="#">Gerencia San Martín</a>
+            <ul class="subMenu">
+                <li><a>U.N Moyobamba</a></li>
+                <li><a>U.N Yurimaguas</a></li>
+                <li><a>U.N Tarapoto</a></li>
+                <li><a>U.N Bellavista</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Gerencia Loreto</a></li>
+        <li><a href="#">Gerencia Amazonas - Cajamarca</a></li>    
+    </ul>
+</div>
